b0b4b3fd6eab9071cc6a01329d6a2815c5029aa5
CodeAstronauth <rjyj_2024@qq.com>
2025-11-21 20:34:50 +0800

    Make title screen

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -50,6 +50,7 @@ public class GamePanel extends JPanel implements Runnable
 	
 	//GAME STATE
 	public int gameState;
+    public final int titleState=0;
 	public final int playState = 1;
 	public final int pauseState = 2;
 	public final int dialogueState = 3;
@@ -69,10 +70,9 @@ public class GamePanel extends JPanel implements Runnable
 	public void setupGame() {
 		
 		aSetter.setObject();
-		playMusic(0);
 		aSetter.setNPC();
-//		stopMusic();
-		gameState = playState;
+//        playMusic(0);
+		gameState = titleState;
 	}
 	
 	public void startGameThread()
@@ -193,31 +193,40 @@ public class GamePanel extends JPanel implements Runnable
 		if(keyH.checkDrawTime == true) {
 			drawStart = System.nanoTime();
 		}
+
+        //TITLE SCREEN
+        if(gameState == titleState){
+            ui.draw(g2);
+        }
+        //OTHERS
+        else{
+            //TILE
+            tileM.draw(g2);
+
+            //OBJECT
+            for (int i = 0; i < obj.length; i++)
+            {
+                if (obj[i] != null) {
+                    obj[i].draw(g2, this);
+                }
+            }
+
+            //NPC
+            for (int i = 0; i < npc.length; i++) {
+                if(npc[i] != null) {
+                    npc[i].draw(g2);
+                }
+            }
+            //PLAYER
+            player.draw(g2);
+
+            //UI
+            ui.draw(g2);
+
+        }
 		
 		
-		//TILE
-		tileM.draw(g2);
-		
-		//OBJECT
-		for (int i = 0; i < obj.length; i++)
-		{
-			if (obj[i] != null) {
-				obj[i].draw(g2, this);
-			}
-		}
-		
-		//NPC
-		for (int i = 0; i < npc.length; i++) {
-			if(npc[i] != null) {
-				npc[i].draw(g2);
-			}
-		}
-		//PLAYER
-		player.draw(g2);
-		
-		//UI
-		ui.draw(g2);
-		
+
 		//DEBUG
 		if (keyH.checkDrawTime == true) {
 			long drawEnd = System.nanoTime();
