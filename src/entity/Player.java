e6ed534e45a1d3444a7c2e42f4074d704e2a8a76
CodeAstronauth <rjyj_2024@qq.com>
2025-11-22 14:47:16 +0800

    create monsters and life reduction mechanism

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,8 +1,6 @@
 package entity;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -103,6 +101,9 @@ public class Player extends Entity{
 			int npcIndex = gp.cChecker.checkEntity(this, gp.npc);
 			interactNPC(npcIndex);
 
+            //CHECK MINSTER COLLISION
+            int monsterIndex = gp.cChecker.checkEntity(this, gp.monster);
+            contactMonster(monsterIndex);
             // CHECK EVENT
             gp.eHandler.checkEvent();
 
@@ -144,7 +145,15 @@ public class Player extends Entity{
 	        }
 		
         }
-		
+
+		// This needs to be outside of key if statement!
+        if(invincible == true) {
+            invincibleCounter++;
+            if(invincibleCounter > 60) {
+                invincible = false;
+                invincibleCounter = 0;
+            }
+        }
 	}
 	
 	public void  pickUpObject(int i) {
@@ -166,6 +175,17 @@ public class Player extends Entity{
 		}
 
 	}
+    public void contactMonster(int i) {
+
+        if(i != 999) {
+
+            if(invincible == false) {
+                life -= 1;
+                invincible = true;
+            }
+
+        }
+    }
 	public void draw(Graphics2D g2)
 	{
 		BufferedImage image = null;
@@ -209,18 +229,20 @@ public class Player extends Entity{
 			break;
 			
 		}
+
+        if(invincible == true) {
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f));
+        }
 		g2.drawImage(image, screenX, screenY, null);
+
+        // Reset alpha
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+
+        //DEBUG
+//        g2.setFont(new Font("Arial", Font.PLAIN, 26));
+//        g2.setColor(Color.white);
+//        g2.drawString("Invincible"+invincibleCounter, 10, 400);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 }
--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -82,52 +82,21 @@ public class CollisionChecker {
 				gp.obj[i].solidArea.y = gp.obj[i].worldY + gp.obj[i].solidArea.y; //you can specify the
 																				//solid Area in each object with the latter part
 				switch(entity.direction) {
-				case "up":
-					entity.solidArea.y -= entity.speed;
-					if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-						if(gp.obj[i].collision == true) {
-							entity.collisionOn = true;
-							}
-						if (player == true) {
-							index = i;
-						}
-					}
-					break;
-				case "down":
-					entity.solidArea.y += entity.speed;
-					if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-						if(gp.obj[i].collision == true) {
-							entity.collisionOn = true;
-							}
-						if (player == true) {
-							index = i;
-						}
-					}
-					break;
-				case "left":
-					entity.solidArea.x -= entity.speed;
-					if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-						if(gp.obj[i].collision == true) {
-							entity.collisionOn = true;
-							}
-						if (player == true) {
-							index = i;
-						}
-					}
-					break;
-				case "right":
-					entity.solidArea.x += entity.speed;
-					if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-						if(gp.obj[i].collision == true) {
-							entity.collisionOn = true;
-							}
-						if (player == true) {
-							index = i;
-						}
-					}
-					break;
-					
+				case "up": entity.solidArea.y -= entity.speed; break;
+				case "down": entity.solidArea.y += entity.speed; break;
+				case "left": entity.solidArea.x -= entity.speed; break;
+				case "right": entity.solidArea.x += entity.speed; break;
 				}
+
+                if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                    if(gp.obj[i].collision == true) {
+                        entity.collisionOn = true;
+                    }
+                    if (player == true) {
+                        index = i;
+                    }
+                }
+
 				entity.solidArea.x = entity.solidAreaDefaultX;
 				entity.solidArea.y = entity.solidAreaDefaultY;
 				gp.obj[i].solidArea.x = gp.obj[i].solidAreaDefaultX;
@@ -139,7 +108,8 @@ public class CollisionChecker {
 	}
 	//NPC & MONSTER COLLISON
 	public int checkEntity(Entity entity, Entity[] target) {
-int index = 999;
+
+        int index = 999;
 		
 		for (int i = 0; i < target.length; i++) {
 			
@@ -154,36 +124,20 @@ int index = 999;
 				target[i].solidArea.y = target[i].worldY + target[i].solidArea.y; //you can specify the
 																				//solid Area in each object with the latter part
 				switch(entity.direction) {
-				case "up":
-					entity.solidArea.y -= entity.speed;
-					if (entity.solidArea.intersects(target[i].solidArea)) {
-						entity.collisionOn = true;
-							index = i;
-					}
-					break;
-				case "down":
-					entity.solidArea.y += entity.speed;
-					if (entity.solidArea.intersects(target[i].solidArea)) {
-						entity.collisionOn = true;
-							index = i;
-					}
-					break;
-				case "left":
-					entity.solidArea.x -= entity.speed;
-					if (entity.solidArea.intersects(target[i].solidArea)) {
-						entity.collisionOn = true;
-							index = i;
-					}
-					break;
-				case "right":
-					entity.solidArea.x += entity.speed;
-					if (entity.solidArea.intersects(target[i].solidArea)) {
-						entity.collisionOn = true;
-							index = i;
-					}
-					break;
-					
+				case "up": entity.solidArea.y -= entity.speed; break;
+				case "down": entity.solidArea.y += entity.speed; break;
+				case "left": entity.solidArea.x -= entity.speed; break;
+				case "right": entity.solidArea.x += entity.speed; break;
 				}
+
+                if (entity.solidArea.intersects(target[i].solidArea)) {
+                    if(target[i] != entity) {
+                        entity.collisionOn = true;
+                        index = i;
+                    }
+
+                }
+
 				entity.solidArea.x = entity.solidAreaDefaultX;
 				entity.solidArea.y = entity.solidAreaDefaultY;
 				target[i].solidArea.x = target[i].solidAreaDefaultX;
@@ -193,8 +147,11 @@ int index = 999;
 		
 		return index;
 	}
-	public void checkPlayer(Entity entity) {
-		//GET ENTITY'S SOLID AREA POSITION
+	public boolean checkPlayer(Entity entity) {
+
+        boolean contactPlayer = false;
+
+        //GET ENTITY'S SOLID AREA POSITION
 		entity.solidArea.x = entity.worldX + entity.solidArea.x;
 		entity.solidArea.y = entity.worldY + entity.solidArea.y;
 		
@@ -203,36 +160,22 @@ int index = 999;
 		gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y; //you can specify the
 																		//solid Area in each object with the latter part
 		switch(entity.direction) {
-		case "up":
-			entity.solidArea.y -= entity.speed;
-			if (entity.solidArea.intersects(gp.player.solidArea)) {
-				entity.collisionOn = true;			
-			}
-			break;
-		case "down":
-			entity.solidArea.y += entity.speed;
-			if (entity.solidArea.intersects(gp.player.solidArea)) {
-				entity.collisionOn = true;
-			}
-			break;
-		case "left":
-			entity.solidArea.x -= entity.speed;
-			if (entity.solidArea.intersects(gp.player.solidArea)) {
-				entity.collisionOn = true;
-			}
-			break;
-		case "right":
-			entity.solidArea.x += entity.speed;
-			if (entity.solidArea.intersects(gp.player.solidArea)) {
-				entity.collisionOn = true;
-			}
-			break;
-			
+		case "up": entity.solidArea.y -= entity.speed; break;
+		case "down": entity.solidArea.y += entity.speed; break;
+		case "left": entity.solidArea.x -= entity.speed; break;
+		case "right": entity.solidArea.x += entity.speed; break;
 		}
+
+        if (entity.solidArea.intersects(gp.player.solidArea)) {
+            entity.collisionOn = true;
+            contactPlayer = true;
+        }
+
 		entity.solidArea.x = entity.solidAreaDefaultX;
 		entity.solidArea.y = entity.solidAreaDefaultY;
 		gp.player.solidArea.x = gp.player.solidAreaDefaultX;
 		gp.player.solidArea.y = gp.player.solidAreaDefaultY;
+        return contactPlayer;
 	}
 }
 
