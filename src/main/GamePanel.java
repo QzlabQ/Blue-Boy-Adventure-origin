8539c40d444f43cc5e482fb9e5e2715f9a8e9e60
Qwqwqwert <18310112005@163.com>
2025-12-05 00:52:45 +0800

    Remove old "run" method

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -119,48 +119,6 @@ public class GamePanel extends JPanel implements Runnable {
 
     }
 
-    @Override
-    // public void run() {
-    //
-    // double drawInterval = 1000000000/FPS;//160M nano seconds
-    // double nextDrawTime = System.nanoTime() + drawInterval;
-    //
-    // while(gameThread != null)
-    // {
-    //
-    // long currentTime = System.nanoTime();
-    // System.out.println("current time:" + currentTime);
-    //
-    // //1.UPDATE
-    // update();
-    //
-    // //2.DRAW
-    // repaint();
-    //
-    //
-    //
-    //
-    // try {
-    // double remainingTime = nextDrawTime - System.nanoTime();
-    // remainingTime = remainingTime / 1000000;
-    //
-    // if (remainingTime < 0)
-    // {
-    // remainingTime = 0;
-    // }
-    //
-    // Thread.sleep((long) remainingTime);
-    //
-    // nextDrawTime += drawInterval;
-    //
-    // } catch (InterruptedException e) {
-    // // TODO Auto-generated catch block
-    // e.printStackTrace();
-    // }
-    // }
-    //
-    // } // we discarded this method
-
     public void run() {
         double drawInterval = 1000000000 / FPS;
         double delta = 0;
